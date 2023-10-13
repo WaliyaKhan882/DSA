614d6f39f7c1e6c006a682ea8dbac978eff7613a
Waliya Khan <109624668+WaliyaKhan882@users.noreply.github.com>
2023-10-13 08:50:01 +0500

    Update stack.cpp

--- Practice/stack.cpp
+++ Practice/stack.cpp
@@ -1,60 +1,70 @@
 #include <iostream>
 using namespace std;
 
-int insert_stack(int *stack, int value, int top)
+int insert_stack(char *stack, char value, int &top, int size)
 {
-    top++;
-    stack[top] = value;
-    cout << stack[top] << endl;
-    cout << "value inserted" << endl;
-    return top;
+    int retvalue;
+    if (top < size)
+    {
+        stack[top++]=value;
+        retvalue = 1;
+    }
+    else
+    {
+        retvalue = 0;
+    }
+    return retvalue;
 }
-int remove_stack(int *stack, int top)
+int remove_stack(char *stack, int &top)
 {
-    top--;
-    cout << stack[top] << endl;
-    cout << "value popped out" << endl;
-    return top;
+    int retvalue;
+    if (top > 0)
+    {
+        cout << stack[--top] << endl;
+        retvalue = 1;
+    }
+    else
+    {
+        retvalue = 0;
+    }
+    return retvalue;
 }
-void traverse_stack(int *stack, int top)
+void traverse_stack(char *stack, int top)
 {
     cout << "stack elements are: " << endl;
-    for (int i = top; i >-1; i--)
+    for (int i = top-1; i >= 0; i--)
     {
         cout << stack[i] << endl;
     }
 }
 int main()
 {
-    int size, value,top = -1;
+    int size, top = 0;
     cout << "enter size" << endl;
     cin >> size;
-    int *stk=new int[size];
+    char *stk=new char[size];
+    char value;
     while (1)
     {
         cout << "enter 1 for insertion in stack || 2 for removing from stack || 3 for printing stack || 0 to quit program" << endl;
-        int option,indicator=0;
+        int option, indicator = 0, result;
         cin >> option;
         switch (option)
         {
         case 1:
             cout << "enter value to be inserted" << endl;
             cin >> value;
-            if(top<size-1){
-            indicator=insert_stack(stk, value, top);
-            top=indicator;
-            }
-            else{
-                cout<<"stack overflow"<<endl;
+            result = insert_stack(stk, value, top, size);
+            if (result == 0)
+            {
+                cout << "stack is full" << endl;
             }
             break;
         case 2:
-            if(top>-1){
-            indicator=remove_stack(stk, top);
-            top=indicator;
-            }
-            else{
-                cout<<"stack underflow"<<endl;
+            result = remove_stack(stk, top);
+            if (result == 0)
+            {
+                cout << "stack underflow" << endl;
             }
             break;
         case 3:
